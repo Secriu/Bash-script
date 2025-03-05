c251ae52f8af931adaae3753305b4f6b4c5aa65f
Secriu <arthur.yuta.raymond@icloud.com>
2025-03-05 11:46:08 +0100

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -4,8 +4,18 @@ $exit = 1
 while ["$exit" = 1] :
 do
 read command
-if [ "$command" = exit]
+if [ "$command" = "exit" ]
 $exit = 0
+fi
+if [ "$command" = "cd" ]
+echo "directory:"
+read $dir
+cd $dir
+fi
+if [ "$command" = "createfile" ]
+echo "name:"
+read $file
+touch $file
 fi
 done
 echo "bye"
